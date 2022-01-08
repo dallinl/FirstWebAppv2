7e90a841f465b4ac79386a700cc8c1f6ff4dea73
dallinl <dladle99@gmail.com>
2022-01-08 16:05:45 -0700

    Add project files.

---
+++ FirstWebAppv2/wwwroot/js/FainMail.js
@@ -0,0 +1,13 @@
+﻿////document.getElementById("btnSend").addEventListener("click", function () {
+////    alert(document.getElementById("From").value + " sent a message about: " +
+////        document.getElementById("Subject").value + ". It said: " +
+////        document.getElementById("Message").value + ". ")
+////})
+
+$("#btnSend").click(function () {
+    alert("This message if from " + $("#From").val() +
+        " sent a message about: " + $("#Subject").val() +
+        " with a message saying " + $("#Message").val());
+
+    $("#GandyHeadShot").fadeOut
+})
